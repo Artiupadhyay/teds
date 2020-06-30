10c230141defac8c0363a05301d4dce54f0f312b
arti upadhyay <artiupadhyay28@gmail.com>
2020-06-30 09:49:49 -0600

    final

--- src/components/cardhalf.jsx
+++ src/components/cardhalf.jsx
@@ -3,16 +3,17 @@ import React from 'react';
 
 function CardHalf(props) {
   return(
-    <div className="card">
-    <div className="wrapper">
-      <iframe className="w-100 h-80" src={props.videoUrl} frameborder="0"
-       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-  <div className="card-body">
-    <h5 className="card-title">{props.heading}</h5>
-    <p className="card-text"> A webinar with {props.name}
-    {props.text}
-    </p>
-</div>
+    <div className="d-flex flex-column mt-5">
+    <div className="wrapper row d-flex justify-content-between  rounded border border-1">
+        <iframe className="w-100 h-100" src={props.videoUrl} frameborder="0"
+        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        </iframe>
+      </div>
+    <div className="wrapper row d-flex justify-content-between border border-1">
+      <h5 className="card-title">{props.heading}</h5>
+      <p className="card-text"> A webinar with {props.name}
+      {props.text}
+      </p>
     </div>
     </div>
       
--- src/components/cardtext.jsx
+++ src/components/cardtext.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 function CardText(props) {
     return(
-        <div class="d-flex align-items-start flex-column">
-  <div class="mb-3 mt-5">
+        <div className=" mt-3 d-flex flex-row  rounded border border-1 ">
+        <div className=" p-5 col-md-12 ">
             <strong><h2 align="center">{props.heading}</h2></strong>
             {props.text}
             </div>
             </div>
+            
 
     );
 }
